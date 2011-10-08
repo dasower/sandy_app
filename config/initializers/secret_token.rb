c07c0a25e93090e3d1d58b568e10a387db4ad486
unknown <Dave@.(none)>
2011-10-08 11:38:15 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandyApp::Application.config.secret_token = '2b196dc15c334f3d3241691f10a7043307c445e4c9e20508159a047f6d7bf67ac1b4347e67b1613e42865153f725d991a027a1b304d6bd1e7774e5fdd857ae69'
